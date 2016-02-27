1ddcec81c2d52ffd711040fd3b5ac22f59a1c8c2
Ernesto Freyre Gonzalez <ernestofreyreg@gmail.com>
2016-02-27 12:34:44 -0800

    more

--- src/components/PatientData.js
+++ src/components/PatientData.js
@@ -6,7 +6,14 @@ export default class PatientData extends React.Component {
   render() {
     return (
       <div className="PatientData">
+        <div className="row-fluid">
+          <div className="col-sm-4">
+            <img className="img-circle" src="http://placehold.it/140x140"/>
+          </div>
+          <div className="col-sm-8">
 
+          </div>
+        </div>
       </div>
     );
   }
