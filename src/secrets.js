e31798a4060ca742caa6f668fcfd9078f2b0598b
Ernesto Freyre Gonzalez <ernestofreyreg@gmail.com>
2016-02-27 18:01:27 -0800

    progress

--- src/secrets.js
+++
@@ -1,3 +0,0 @@
-export const API_AI_ACCESS_TOKEN = "75f2169e2d0b4212a45b073fb6dadc81";
-export const API_AI_SUBSCRIPTION_KEY = "89c5bb7b-e1ed-4f2e-b7ac-e276ab718842";
-export const API_AI_BASE_URL = "https://api.api.ai/v1/";
